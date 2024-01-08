740e6162532b9d141197c4e418efdd4acf5943c6
b3rz3rkr <22635858+b3rz3rkr@users.noreply.github.com>
2024-01-08 15:09:38 +0200

    fixed dependency

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,7 +2,7 @@ import { Component, createElement, createRef } from 'react'
 import PropTypes from 'prop-types'
 import CircularProgress from '@mui/material/CircularProgress'
 import { common } from '@mui/material/colors'
-import MdBrokenImage from 'react-icons'
+import { MdBrokenImage } from 'react-icons/md'
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
